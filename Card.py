4dbdf63b2950aa813238c462aee54f40417860e7
mjoze <mirek.jozefiak@gmail.com>
2020-02-06 21:43:56 +0100

    '0602'

--- Card.py
+++ Card.py
@@ -11,14 +11,24 @@ class Card:
         return rep
 
 
-card1 = Card('A', "c")
-card2 = Card('5', "d")
-card3 = Card('4', "h")
-card4 = Card('3', "h")
-card5 = Card('2', "s")
+class UnprintableCard(Card):
+    def __str__(self):
+        return "<utajniona>"
 
-print(card1)
-print(card2)
-print(card3)
-print(card4)
-print(card5)
+
+class PositionableCard(Card):
+    def __init__(self, rank, suit, face_up=True):
+        super().__init__(rank, suit)
+        self.is_face_up = face_up
+
+    def __str__(self):
+        if self.is_face_up:
+            rep = super().__str__()
+        else:
+            rep = "XX"
+        return rep
+
+    def flip(self):
+        self.is_face_up = not self.is_face_up
+
+
--- Hand.py
+++ Hand.py
@@ -1,6 +1,3 @@
-import Card
-
-
 class Hand:
     def __init__(self):
         self.cards = []
@@ -25,27 +22,3 @@ class Hand:
         other_hand.add(card)
 
 
-my_hand = Hand()
-print("\nWyświetlam zawartość mojej ręki przed dodaniem jakichkoliwek kart:")
-print(my_hand)
-my_hand.add(Card.card1)
-my_hand.add(Card.card2)
-my_hand.add(Card.card3)
-my_hand.add(Card.card4)
-my_hand.add(Card.card5)
-print(my_hand)
-
-your_hand = Hand()
-my_hand.give(Card.card1, your_hand)
-my_hand.give(Card.card2, your_hand)
-print("\nPrzekazuje pierwsze dwie karty z mojej ręki do Twojej.")
-print("--------------------------")
-print("Twoja ręka:")
-print(your_hand)
-print("Moja ręka:")
-print(my_hand)
-
-my_hand.clear()
-print("\nMoja ręka po usunięciu z niej kart:")
-print(my_hand)
-input("\n\nAby zakończyć program, naciśnij klawisz Enter")

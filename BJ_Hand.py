ac77834f6f90946dbffb7b36f18ab2d64969eeac
mjoze <mirek.jozefiak@gmail.com>
2020-02-07 22:18:15 +0100

    '07.02'

--- BJ_Hand.py
+++ BJ_Hand.py
@@ -1,4 +1,5 @@
 import Hand
+import BJ_Card
 
 
 class BJ_Hand(Hand.Hand):
@@ -11,3 +12,23 @@ class BJ_Hand(Hand.Hand):
         if self.total:
             rep += "(" + str(self.total) + ")"
             return rep
+
+    @property
+    def total(self):
+        for card in self.cards:
+            if not card.value:
+                return None
+        t = 0
+        for card in self.cards:
+            t += card.value
+        contains_ace = False
+        for card in self.cards:
+            if card.value == BJ_Card.BJ_Card.ACE_VALUE:
+                contains_ace = True
+        if contains_ace and t <= 11:
+            t += 10
+        return t
+
+    def is_busted(self):
+        return self.total > 21
+
